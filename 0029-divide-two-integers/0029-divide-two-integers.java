f408ebeb583e61f033f74dfd9f718e1b813abb43
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2024-10-07 22:57:30 +0530

    Time: 0 ms (100%), Space: 41.3 MB (9.42%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,5 +1,11 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+       
+        return Integer.MAX_VALUE;
+    }
+        int q;
+        q=(int) dividend/divisor;
+        return q;
     }
 }
