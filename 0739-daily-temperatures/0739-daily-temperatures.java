e5cada882052a66fa0309a42acfb8630e2624bcd
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2024-12-28 19:01:03 +0530

    Time: 61 ms (76.47%), Space: 63.3 MB (8.27%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -5,7 +5,8 @@ class Solution {
         int res[]=new int[n];
         for(int i=0;i<n;i++){
             while(!s.isEmpty() && temperatures[s.peek()]<temperatures[i]){
-                res[s.peek()]=i-s.pop();
+                int index = s.pop(); 
+                res[index] = i -index;
             }
             s.push(i);
         }
