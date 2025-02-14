cf4019c7d8d22864c466290eedf0f43b297594ff
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2025-02-14 14:46:37 +0530

    Time: 6 ms (22.78%), Space: 45.2 MB (25.29%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public boolean canPlace(int col, int row, List<String> curr) {
+        for (int i = row - 1; i >= 0; i--) {
+            if (curr.get(i).charAt(col) == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (curr.get(i).charAt(j) == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < curr.size(); i--, j++) {
+            if (curr.get(i).charAt(j) == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            curr.add(".".repeat(n));
+        }
+        backtrack(0, curr, res);
+        return res;
+    }
+
+    public void backtrack(int row, List<String> curr, List<List<String>> res) {
+        if (row == curr.size()) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int col = 0; col < curr.size(); col++) {
+            if (canPlace(col, row, curr)) {
+                StringBuilder sb = new StringBuilder(curr.get(row));
+                sb.setCharAt(col, 'Q');
+                curr.set(row, sb.toString());
+                backtrack(row + 1, curr, res);
+                sb.setCharAt(col, '.');
+                curr.set(row, sb.toString());
+            }
+        }
+    }
+}
