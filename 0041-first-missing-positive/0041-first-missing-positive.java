d4f0df88557acf8ada5141c1bea988bec98e8b87
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2025-03-12 15:45:19 +0530

    Time: 1 ms (100%), Space: 57.1 MB (70.54%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,18 +1,19 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> op = new HashSet<>();
-        for(int num : nums)
+        int n= nums.length;
+       for(int i=0;i<n;i++){
+        while(nums[i]>0 && nums[i]<=n && nums[i] != nums[nums[i]-1])
         {
-            if(num>=0)
-            {
-                op.add(num);
-            }
+            int temp=nums[nums[i]-1];
+            nums[nums[i]-1]=nums[i];
+            nums[i]=temp;
         }
-            int m=1;
-            while(op.contains(m))
-            m++;
-
-            return m;
-        
+       }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1)
+            return i+1;
+        }
+       
+        return n+1;
     }
 }
