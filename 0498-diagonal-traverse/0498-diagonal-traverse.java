2d1094b63539d741ddda1b1c4eb66fb036b63a32
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2025-10-21 23:38:41 +0530

    Time: 3 ms (62.33%), Space: 47.5 MB (22.65%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,37 +1,30 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        Map<Integer,ArrayList> map = new HashMap<>();
-        int  r = mat.length;
-        int c = mat[0].length;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                int sum = i+j;
-                if(!map.containsKey(sum)){
-                    map.put(sum,new ArrayList<>());
-                }  
-                map.get(sum).add(mat[i][j]);
+        int n = mat.length;
+        int m = mat[0].length;
+        int res[] = new int[n*m];
+        int i =0;
+        int row = 0,col = 0;
+        for(int d = 0;d<n+m-1;d++){
+            if(d%2==0){
+                row = Math.min(d,n-1);
+                col = d - row;
+                while(row >= 0 && col < m){
+                    res[i++] = mat[row][col];
+                    row--;
+                    col++;
+                }
             }
+            else{
+                    col = Math.min(d,m-1);
+                    row = d - col;
+                    while(col >= 0 && row<n){
+                        res[i++] = mat[row][col];
+                        row++;
+                        col--;
+                    }
+                }
         }
-        int res[] = new int[r*c];
-        int itr=0;
-        int itr2=0;
-        for(int i:map.keySet()){
-            ArrayList<Integer> list = map.get(i);
-            if(itr%2==0)
-            {
-                Collections.reverse(list);
-                
-            }
-            
-            for(int j:list){
-                res[itr2]=j;
-            itr2++;
-            }
-            
-
-
-        itr++;              
-        }
-        return res;
+            return res;
     }
 }
