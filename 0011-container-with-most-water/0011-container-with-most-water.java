4ea67a0af29267c8431070726a042bc6feebeba8
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2025-12-04 06:28:06 +0530

    Time: 5 ms (77.67%), Space: 77.4 MB (14.04%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,30 +1,20 @@
 class Solution {
     public int maxArea(int[] height) {
-        int l = 0;
-        int r = height.length-1;
-        int max = 0 ;
-        int n =  height.length-1;
-
-        // two pointer Approach
-
-        // TC: O(n)
-        // SC: O(1)
-        while(l < n && r >= 0){
+        int n = height.length;
+        int l = 0,r = n-1;
+        int max =0;
+        while(l<r){
             int h1 = height[l];
             int h2 = height[r];
 
-            int h = Math.min(h1,h2);
+            int min = Math.min(h1,h2);
 
             int width = r - l;
 
-            int area = h * width;
-
-            max = Math.max(max,area);
-            //System.out.println(area);
-            if(h1<h2)
-            l++;
-            else
-            r--;
+            int area  = min * width;
+            max = Math.max(max , area);
+            if(h1 < h2) l++;
+            else r--;
         }
         return max;
     }
