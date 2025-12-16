1b3ab9cb562d004f48648e2fc955aa36e413baac
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2025-12-16 10:06:53 +0530

    Time: 2 ms (99.87%), Space: 52.4 MB (35.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,30 @@
 class Solution {
+    public int res = 0;
     public int numIslands(char[][] grid) {
-        if(grid==null||grid.length==0) return 0;
-        int r = grid.length;
-        int c = grid[0].length;
-        int num = 0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(grid[i][j]=='1'){
+        int n = grid.length;
+        int m = grid[0].length;
+        if(grid == null || n == 0) return 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]== '1'){
                     dfs(grid,i,j);
-                    num++;
+                    res++;
                 }
             }
         }
-        return num;
+        return res;
     }
-    public void dfs(char grid[][],int r,int c){
+    public void dfs(char[][] grid,int r,int c){
         int n = grid.length;
         int m = grid[0].length;
+        if(r<0 || r>=n || c < 0 || c >= m || grid[r][c] == '0'){
+            return;
+        }
+        grid[r][c] = '0';
 
-        if(r<0 || c<0 || r>=n ||c>=m || grid[r][c]== '0') return;
-        grid[r][c]='0';
-        dfs(grid,r-1,c);
         dfs(grid,r+1,c);
-        dfs(grid,r,c-1);
+        dfs(grid,r-1,c);
         dfs(grid,r,c+1);
+        dfs(grid,r,c-1);
     }
-
 }
