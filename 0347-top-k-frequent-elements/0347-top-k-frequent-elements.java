b68e73ceb3cce72ff591f51bca55d5810fa477d0
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2025-12-16 09:39:03 +0530

    Time: 11 ms (93.91%), Space: 47.4 MB (78.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,17 +4,23 @@ class Solution {
         for(int i:nums){
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        ArrayList<Integer> res =new ArrayList<>();
-
-
-        for(Map.Entry<Integer,Integer> i:map.entrySet()){
-            if(i.getValue() >= k){
-                res.add(i.getKey());
-            }
+        List<Integer>[] bucket =new List[nums.length+1];
+        for(int key : map.keySet()){
+            int f = map.get(key);
+            if(bucket[f]==null)
+                bucket[f] = new ArrayList<>();
+            bucket[f].add(key);
         }
-        int r[] = new int[res.size()];
-        for(int i=0;i<res.size();i++){
-            r[i] = res.get(i);
+        
+        int r[] = new int[k];
+        int idx =0;
+        for(int i = bucket.length-1;i>=0 && idx<k;i--){
+            if(bucket[i]!=null){
+                for(int num:bucket[i]){
+                    r[idx++]=num;
+                    if(idx==k) break;
+                }
+            }
         }
         return r;
     }
