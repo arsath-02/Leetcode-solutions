aa2029bb72c8b2a79cf4f2bd6a8ca695d6584c1b
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2026-01-15 07:59:44 +0530

    Time: 0 ms (100%), Space: 43.8 MB (6.1%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,20 +1,19 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
-        int dp[][] = new int[n][m];
-        for(int i[] :dp){
-            Arrays.fill(i,-1);
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int dp[] = new int[m];
+        dp[0] = 1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1){
+                    dp[j] = 0;
+                }
+                else if(j>0){
+                    dp[j] = dp[j] +dp[j-1];
+                }
+            }
         }
-        return backtrack(0,0,n,m,dp,obstacleGrid);
-         
-    }
-    public int  backtrack(int r,int c,int n ,int m ,int dp[][],int grid[][]){
-        if(r>=n || c>=m || r<0 || c<0 || grid[r][c] == 1){
-            return 0;
-        }
-        if(r == n-1 && c == m-1) return 1;
-        if(dp[r][c]!= -1) return dp[r][c];
-        return dp[r][c] = backtrack(r+1,c,n,m,dp,grid) + backtrack(r,c+1,n,m,dp,grid);
+        return dp[m-1];
     }
 }
